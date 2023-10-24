b45a03ebd83ad113b743e632a5ea4cdf685127ee
Beenish Khan <beenishkhan603@gmail.com>
2023-10-24 13:23:25 +0500

    Add: get api to fetch newyork times stories

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/top-stories', async (req, res) => {
+	try {
+		const response = await axios.get(
+			`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.NY_API_KEY}`
+		);
+		res.json(response.data);
+	} catch (error) {
+		console.error('Error fetching data from New York Times API:', error);
+		res.status(500).json({ error: 'An error occurred' });
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
